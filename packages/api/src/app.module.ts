5f5e289abd33e305a92acf076983e121122e1eae
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-08-17 21:24:07 -0700

    feat(app):  add GraphQLModule

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { join } from 'path'
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ['../gql/schema.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/common/types/graphql.ts'),
+        outputAs: 'class'
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
