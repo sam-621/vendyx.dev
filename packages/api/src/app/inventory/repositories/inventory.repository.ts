14bbd79c6b9538735a78a71f7d174f9b6f385a77
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-08-22 22:25:26 -0700

    feat(inventory):  add InventoryRepository

--- packages/api/src/app/inventory/repositories/inventory.repository.ts
+++ packages/api/src/app/inventory/repositories/inventory.repository.ts
@@ -1,5 +1,5 @@
 import { PrismaService } from '@/app/shared/services'
-import { Product } from '@/common/types/graphql'
+import { Asset, AssetType, Collection, LabelValues, Option, Product } from '@/common/types/graphql'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
@@ -13,9 +13,55 @@ export class InventoryRepository {
   async findOne(id: string): Promise<ProductWithNoRelations> {
     return this.prismaService.product.findUnique({ where: { id } })
   }
+
+  async getCollectionsOnProduct(id: string): Promise<CollectionWithNoRelations[]> {
+    const result = await this.prismaService.productOnCollection.findMany({
+      where: { productId: id },
+      include: { collection: true }
+    })
+
+    return result.map(r => r.collection)
+  }
+
+  async getAssetsOnProduct(id: string): Promise<AssetWithNoRelations[]> {
+    const result = await this.prismaService.assetOnProduct.findMany({
+      where: { productId: id },
+      include: { asset: true }
+    })
+
+    return result.map(r => ({ ...r.asset, type: r.asset.type as AssetType }))
+  }
+
+  async getLabelValuesOnProduct(id: string): Promise<LabelValuesWithNoRelations[]> {
+    const result = await this.prismaService.labelValueOnProduct.findMany({
+      where: { productId: id },
+      include: { labelValue: true }
+    })
+
+    return result.map(r => r.labelValue)
+  }
+
+  async getOptionsOnProduct(id: string): Promise<OptionWithNoRelations[]> {
+    const result = await this.prismaService.option.findMany({
+      where: { productId: id }
+    })
+
+    return result
+  }
 }
 
 type ProductWithNoRelations = Omit<
   Product,
   'variants' | 'collections' | 'assets' | 'labelValues' | 'options'
 >
+
+type CollectionWithNoRelations = Omit<Collection, 'products' | 'assets' | 'labelValues'>
+
+type AssetWithNoRelations = Omit<
+  Asset,
+  'products' | 'collections' | 'productVariants' | 'labelValues'
+>
+
+type LabelValuesWithNoRelations = Omit<LabelValues, 'label'>
+
+type OptionWithNoRelations = Omit<Option, 'values'>
--- packages/api/src/app/inventory/resolvers/inventory.resolver.ts
+++ packages/api/src/app/inventory/resolvers/inventory.resolver.ts
@@ -14,44 +14,21 @@ export class InventoryResolver {
 
   @ResolveField('collections')
   async collections(@Parent() product: Product) {
-    const { id } = product
-    const result = await this.prismaService.productOnCollection.findMany({
-      where: { productId: id },
-      include: { collection: true }
-    })
-
-    return result.map(r => r.collection)
+    return this.repository.getCollectionsOnProduct(product.id)
   }
 
   @ResolveField('assets')
   async assets(@Parent() product: Product) {
-    const { id } = product
-    const result = await this.prismaService.assetOnProduct.findMany({
-      where: { productId: id },
-      include: { asset: true }
-    })
-
-    return result.map(r => r.asset)
+    return this.repository.getAssetsOnProduct(product.id)
   }
 
   @ResolveField('labelValues')
   async labelValues(@Parent() product: Product) {
-    const { id } = product
-    const result = await this.prismaService.labelValueOnProduct.findMany({
-      where: { productId: id },
-      include: { labelValue: true }
-    })
-
-    return result.map(r => r.labelValue)
+    return this.repository.getLabelValuesOnProduct(product.id)
   }
 
   @ResolveField('options')
   async options(@Parent() product: Product) {
-    const { id } = product
-    const result = await this.prismaService.option.findMany({
-      where: { productId: id }
-    })
-
-    return result
+    return this.repository.getOptionsOnProduct(product.id)
   }
 }
