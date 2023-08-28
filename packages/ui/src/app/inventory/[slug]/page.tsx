e8dee6e6a06de3dd44e1982b602df65d23bdcc5f
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-08-27 21:57:42 -0700

    feat(inventory):  add sluyg page in inventorty

---
+++ packages/ui/src/app/inventory/[slug]/page.tsx
@@ -0,0 +1,8 @@
+export default async function CreateProductPage() {
+  return (
+    <div className="flex flex-col gap-8">
+      <h1 className="text-foreground font-semibold text-4xl">Create product</h1>
+      <section></section>
+    </div>
+  )
+}
