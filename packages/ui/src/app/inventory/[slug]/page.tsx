56c58300d9a14f42408580494e1a633aaef7bff5
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-08-30 21:44:26 -0700

    feat(inventory):  add Breadcrumb component

--- packages/ui/src/app/inventory/[slug]/page.tsx
+++ packages/ui/src/app/inventory/[slug]/page.tsx
@@ -1,10 +1,18 @@
 import { ProductDetails } from '@/components/inventory'
+import { Breadcrumb } from '@/theme/components'
+import { Button } from '@nextui-org/button'
 
 export default async function CreateProductPage() {
   return (
-    <div className="flex flex-col gap-8">
-      <h1 className="text-foreground font-semibold text-4xl">Create product</h1>
-      <ProductDetails />
+    <div className="flex flex-col gap-4">
+      <Breadcrumb items={[{ label: 'Inventory', href: '/inventory' }, { label: 'Silloncito' }]} />
+      <div className="flex flex-col gap-8">
+        <div className="flex items-center justify-between">
+          <h1 className="text-foreground font-semibold text-4xl">Create product</h1>
+          <Button color="primary">Save product</Button>
+        </div>
+        <ProductDetails />
+      </div>
     </div>
   )
 }
---
+++ packages/ui/src/ui/theme/components/breadcrumb.tsx
@@ -0,0 +1,45 @@
+import { ChevronRightIcon } from '@heroicons/react/24/outline'
+import { ChartBarIcon } from '@heroicons/react/24/solid'
+import Link from 'next/link'
+import type { FC } from 'react'
+
+export const Breadcrumb: FC<Props> = ({ items }) => {
+  return (
+    <nav className="flex gap-3 items-center">
+      <Link
+        href="/"
+        className="flex items-center gap-1 text-sm text-default-500 font-medium hover:text-white transition-colors"
+      >
+        <ChartBarIcon width={16} />
+        Home
+      </Link>
+      <ChevronRightIcon width={16} className="text-default-500" />
+      {items.map(item => (
+        <>
+          {item.href !== undefined ? (
+            <>
+              <Link
+                href={item.href}
+                className="text-sm text-default-500 font-medium hover:text-white transition-colors"
+              >
+                {item.label}
+              </Link>
+              <ChevronRightIcon width={16} className="text-default-500" />
+            </>
+          ) : (
+            <span className="text-sm text-default-500 font-medium hover:text-white transition-colors">
+              Silloncito
+            </span>
+          )}
+        </>
+      ))}
+    </nav>
+  )
+}
+
+type Props = {
+  items: {
+    label: string
+    href?: string
+  }[]
+}
