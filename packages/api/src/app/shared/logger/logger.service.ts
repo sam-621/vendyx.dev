42f733209abf81fe19bc0b7535095135d7017acc
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-09-30 21:44:11 -0700

    feat(shaered):  add logger module

---
+++ packages/api/src/app/shared/logger/logger.service.ts
@@ -0,0 +1,14 @@
+import { ConsoleLogger, Injectable, Scope } from '@nestjs/common'
+import { ErrorCode } from '../errors'
+
+@Injectable({ scope: Scope.TRANSIENT })
+export class LoggerService extends ConsoleLogger {
+  businessLog(error: Error, errorCode: ErrorCode, message: string[], stack?: boolean) {
+    this.log({
+      error: `${error.name}: ${error.message}`,
+      errorCode,
+      message,
+      trace: stack ? error.stack : undefined
+    })
+  }
+}
