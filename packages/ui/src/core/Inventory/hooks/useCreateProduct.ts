ebffc4cdc94ddd1582f00a2de22f2a58e897c9de
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-10-12 22:22:24 -0700

    feat(inventory):  update create product

--- packages/ui/src/core/Inventory/hooks/useCreateProduct.ts
+++ packages/ui/src/core/Inventory/hooks/useCreateProduct.ts
@@ -15,7 +15,13 @@ export const useCreateProduct = (): Return => {
 export const createProductValidator = z.object({
   name: z.string().min(3, 'El nombre debe de ser mayor a 3 caracteres'),
   description: z.string(),
-  images: z.any(),
+  images: z.preprocess(value => {
+    const fileList = value as FileList
+
+    if (fileList.item === undefined) throw new Error('The input value is not an image')
+
+    return fileList.item(1)
+  }, z.any()),
   price: z.preprocess(value => Number(value), z.number()),
   offerPrice: z.preprocess(value => Number(value), z.number()),
   costPerProduct: z.preprocess(value => Number(value), z.number()),
