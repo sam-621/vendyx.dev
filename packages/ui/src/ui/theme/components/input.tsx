99e2041b216ef40b6032cf3a03fafde2942a2fa1
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-10-15 23:55:20 -0700

    feat(inventopry):  update input

--- packages/ui/src/ui/theme/components/input.tsx
+++ packages/ui/src/ui/theme/components/input.tsx
@@ -11,12 +11,15 @@ const Input = React.forwardRef<HTMLInputElement, Props>(
           type={type}
           className={cn(
             'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50',
+            errorMessage !== undefined && errorMessage.length > 0 ? 'border-destructive' : '',
             className
           )}
           ref={ref}
           {...props}
         />
-        <span className="text-sm font-medium text-destructive">{errorMessage}</span>
+        {errorMessage !== undefined && (
+          <span className="text-sm font-medium text-destructive">{errorMessage}</span>
+        )}
       </div>
     )
   }
