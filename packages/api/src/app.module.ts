1fca833be25eb0f903454675a2a1dc38bab0d342
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-10-24 23:36:55 -0700

    feat(shared):  add graphql module

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,32 +1,12 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { GraphQLModule } from '@nestjs/graphql'
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
-import { join } from 'path'
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
+
 import { SharedModule } from './app/shared'
 import { InventoryModule } from './app/inventory'
-import { LoggerModule } from './app/shared/logger'
-import { ConfigModule } from './app/shared/config'
 
 @Module({
-  imports: [
-    ConfigModule,
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      playground: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      typePaths: ['./**/*.gql'],
-      definitions: {
-        path: join(process.cwd(), 'src/common/types/graphql.ts'),
-        outputAs: 'class'
-      }
-    }),
-    LoggerModule,
-    SharedModule,
-    InventoryModule
-  ],
+  imports: [SharedModule, InventoryModule],
   controllers: [AppController],
   providers: [AppService]
 })
--- packages/api/src/app/shared/shared.module.ts
+++ packages/api/src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
 import { Global, Module } from '@nestjs/common'
 import { PrismaModule } from './persistance/prisma.module'
 import { ScalarsModule } from './scalars/scalars.module'
+import { LoggerModule } from './logger'
+import { ConfigModule } from './config'
+import { GraphQLModule } from './gql'
 
 @Global()
 @Module({
-  imports: [PrismaModule, ScalarsModule]
+  imports: [ConfigModule, PrismaModule, ScalarsModule, LoggerModule, GraphQLModule]
 })
 export class SharedModule {}
