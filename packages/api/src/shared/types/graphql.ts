9056eb1599ab17e98c2c092277c26a53c1d1c8f7
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-10-31 14:30:11 -0700

    feat(assets):  add asset api reference

--- packages/api/src/shared/types/graphql.ts
+++ packages/api/src/shared/types/graphql.ts
@@ -8,6 +8,11 @@
 /* tslint:disable */
 /* eslint-disable */
 
+export enum AssetType {
+    IMAGE = "IMAGE",
+    FILE = "FILE"
+}
+
 export interface Node {
     id: string;
     createdAt: Date;
@@ -16,6 +21,54 @@ export interface Node {
 
 export interface List {
     items: Nullable<Node>[];
+    totalItems: number;
+}
+
+export class Asset implements Node {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    name: string;
+    source: string;
+    type: AssetType;
+    enable: boolean;
+    products: Nullable<ProductList>[];
+    categories: Nullable<CategoryList>[];
+}
+
+export class AssetList implements List {
+    items: Nullable<Asset>[];
+    totalItems: number;
+}
+
+export abstract class IQuery {
+    abstract assets(): AssetList | Promise<AssetList>;
+
+    abstract asset(id?: Nullable<string>): Nullable<Asset> | Promise<Nullable<Asset>>;
+
+    abstract categories(): CategoryList | Promise<CategoryList>;
+
+    abstract category(id?: Nullable<string>, slug?: Nullable<string>): Nullable<Category> | Promise<Nullable<Category>>;
+
+    abstract products(): ProductList | Promise<ProductList>;
+
+    abstract product(id?: Nullable<string>, slug?: Nullable<string>): Nullable<Product> | Promise<Nullable<Product>>;
+}
+
+export class Category implements Node {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    name: string;
+    slug: string;
+    description?: Nullable<string>;
+    enable: boolean;
+    products: Nullable<ProductList>[];
+    assets: Nullable<AssetList>[];
+}
+
+export class CategoryList implements List {
+    items: Nullable<Category>[];
     totalItems: number;
 }
 
@@ -46,17 +99,13 @@ export class Product implements Node {
     description?: Nullable<string>;
     enable: boolean;
     variants: Nullable<ProductVariantList>[];
+    categories: Nullable<CategoryList>[];
+    assets: Nullable<AssetList>[];
 }
 
 export class ProductList implements List {
     items: Nullable<Product>[];
     totalItems: number;
-}
-
-export abstract class IQuery {
-    abstract products(): ProductList | Promise<ProductList>;
-
-    abstract product(id?: Nullable<string>, slug?: Nullable<string>): Nullable<Product> | Promise<Nullable<Product>>;
 }
 
 export class PaginatedListInput {
