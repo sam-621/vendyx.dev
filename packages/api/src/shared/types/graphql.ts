ff9137d0c249491a77cba42b10c751420a7715df
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-10-31 14:14:05 -0700

    feat(inventory):  add inventory graphql api

--- packages/api/src/shared/types/graphql.ts
+++ packages/api/src/shared/types/graphql.ts
@@ -19,4 +19,50 @@ export interface List {
     totalItems: number;
 }
 
+export class ProductVariant implements Node {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    sku?: Nullable<string>;
+    price: number;
+    comparisonPrice?: Nullable<number>;
+    costPerUnit: number;
+    weight?: Nullable<number>;
+    stock: number;
+    enable: boolean;
+}
+
+export class ProductVariantList implements List {
+    items: Nullable<ProductVariant>[];
+    totalItems: number;
+}
+
+export class Product implements Node {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    name: string;
+    slug: string;
+    description?: Nullable<string>;
+    enable: boolean;
+    variants: Nullable<ProductVariantList>[];
+}
+
+export class ProductList implements List {
+    items: Nullable<Product>[];
+    totalItems: number;
+}
+
+export abstract class IQuery {
+    abstract products(): ProductList | Promise<ProductList>;
+
+    abstract product(id?: Nullable<string>, slug?: Nullable<string>): Nullable<Product> | Promise<Nullable<Product>>;
+}
+
+export class PaginatedListInput {
+    skip?: Nullable<number>;
+    first?: Nullable<number>;
+    last?: Nullable<number>;
+}
+
 type Nullable<T> = T | null;
