70c04af09a5b4f001ea8c7e3a8453584edb388e2
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-11-05 22:15:21 -0700

    feat(inventory):  add queries for inventopry

---
+++ packages/ui/src/core/inventory/gql/queries.ts
@@ -0,0 +1,23 @@
+import { BasicProductFragment, DetailedProductFragment } from './fragments';
+
+export const GetInventoryProductsQuery = /* GraphQL */ `
+  ${BasicProductFragment}
+
+  query GetInventoryProducts {
+    products {
+      items {
+        ...BasicProduct
+      }
+    }
+  }
+`;
+
+export const GetProductDetailsQuery = /* GraphQL */ `
+  ${DetailedProductFragment}
+
+  query GetProductDetails($slug: String!) {
+    product(slug: $slug) {
+      ...DetailedProduct
+    }
+  }
+`;
