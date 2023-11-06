2c8404dc684db9f3567719856498d64bf48a48cf
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-11-05 22:11:07 -0700

    feat(shared):  add gql request

---
+++ packages/ui/src/core/shared/gql/fragments.ts
@@ -0,0 +1,32 @@
+export const CommonAssetFragment = /* GraphQL */ `
+  fragment CommonAsset on Asset {
+    id
+    source
+  }
+`;
+
+export const CommonVariantFragment = /* GraphQL */ `
+  fragment CommonVariant on ProductVariant {
+    id
+    price
+    stock
+    sku
+  }
+`;
+
+export const CommonCollectionFragment = /* GraphQL */ `
+  fragment CommonCollection on Collection {
+    id
+    name
+    slug
+  }
+`;
+export const CommonProductFragment = /* GraphQL */ `
+  fragment CommonProduct on Product {
+    id
+    createdAt
+    name
+    slug
+    enabled
+  }
+`;
