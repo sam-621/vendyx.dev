15a6f4d0c64016084631eb4e15c54896bf1b2398
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-11-05 18:36:26 -0700

    feat(eslint):  add simple-import-sort rulew

--- packages/ui/src/ui/components/lists/app-sidebar.tsx
+++ packages/ui/src/ui/components/lists/app-sidebar.tsx
@@ -1,12 +1,5 @@
 import clsx from 'clsx';
-import {
-  BarChart2Icon,
-  BoxesIcon,
-  PackageIcon,
-  ShoppingCartIcon,
-  TagIcon,
-  UserIcon
-} from 'lucide-react';
+import { BarChart2Icon, BoxesIcon, PackageIcon, ShoppingCartIcon, UserIcon } from 'lucide-react';
 import Link from 'next/link';
 import { type FC } from 'react';
 
@@ -15,28 +8,28 @@ export const AppSidebar: FC<Props> = async ({ className }) => {
     <aside className={clsx('border-r h-full py-8 px-4', className)}>
       <div className="flex flex-col gap-6">
         <div className="flex flex-col gap-2">
-          <h2 className="text-lg font-semibold tracking-tight pl-4">Catálogo</h2>
+          <h2 className="text-lg font-semibold tracking-tight pl-4">Catalgo</h2>
           <div className="flex flex-col gap-1">
             <Link
               href={'/'}
               className="px-4 py-2 items-center flex gap-2 w-full justify-start hover:bg-secondary transition-colors rounded-md text-sm font-medium"
             >
               <BarChart2Icon className="flex-shrink-0" size={16} />
-              Panel
+              Dashboard
             </Link>
             <Link
               href={'/inventory'}
               className="px-4 py-2 items-center flex gap-2 w-full justify-start hover:bg-secondary transition-colors rounded-md text-sm font-medium"
             >
               <PackageIcon className="flex-shrink-0" size={16} />
-              Inventario
+              Inventory
             </Link>
             <Link
               href={'/collections'}
               className="px-4 py-2 items-center flex gap-2 w-full justify-start hover:bg-secondary transition-colors rounded-md text-sm font-medium"
             >
               <BoxesIcon className="flex-shrink-0" size={16} />
-              Colecciones
+              Collections
             </Link>
           </div>
         </div>
@@ -48,21 +41,14 @@ export const AppSidebar: FC<Props> = async ({ className }) => {
               className="px-4 py-2 items-center flex gap-2 w-full justify-start hover:bg-secondary transition-colors rounded-md text-sm font-medium"
             >
               <ShoppingCartIcon className="flex-shrink-0" size={16} />
-              Ordenes
+              Orders
             </Link>
             <Link
               href={'/customers'}
               className="px-4 py-2 items-center flex gap-2 w-full justify-start hover:bg-secondary transition-colors rounded-md text-sm font-medium"
             >
               <UserIcon className="flex-shrink-0" size={16} />
-              Clientes
-            </Link>
-            <Link
-              href={'/cupons'}
-              className="px-4 py-2 items-center flex gap-2 w-full justify-start hover:bg-secondary transition-colors rounded-md text-sm font-medium"
-            >
-              <TagIcon className="flex-shrink-0" size={16} />
-              Cupones
+              Customers
             </Link>
           </div>
         </div>
