140e4b0f59ef41268f328de9a50478cd374aa90f
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-11-05 22:19:53 -0700

    feat(inventory):  add services

---
+++ packages/ui/src/core/inventory/services/products.ts
@@ -0,0 +1,31 @@
+import { gqlRequest } from '@/core/shared/gql';
+
+import { GetInventoryProductsQuery, GetProductDetailsQuery } from '../gql/queries';
+import type {
+  GetInventoryProductsQueryResult,
+  GetProductDetailsArgs,
+  GetProductDetailsQueryResult
+} from '../types/queries';
+
+type GetProductsResponse = { products: GetInventoryProductsQueryResult };
+
+export const getProducts = async (): Promise<GetInventoryProductsQueryResult> => {
+  const { products } = await gqlRequest<GetProductsResponse>({
+    query: GetInventoryProductsQuery
+  });
+
+  return products;
+};
+
+type GetProductDetailsResponse = { product: GetProductDetailsQueryResult | null };
+
+export const getProductDetails = async (
+  args: GetProductDetailsArgs
+): Promise<GetProductDetailsQueryResult | null> => {
+  const { product } = await gqlRequest<GetProductDetailsResponse, GetProductDetailsArgs>({
+    query: GetProductDetailsQuery,
+    variables: args
+  });
+
+  return product;
+};
---
+++ packages/ui/src/core/inventory/types/queries.ts
@@ -0,0 +1,13 @@
+import type { List } from '@/core/shared/types';
+
+import type { BasicProduct, DetailedProduct } from './product';
+
+// Get inventory products
+export type GetInventoryProductsQueryResult = List<BasicProduct>;
+
+// Get product details
+export type GetProductDetailsQueryResult = DetailedProduct;
+
+export type GetProductDetailsArgs = {
+  slug: string;
+};
