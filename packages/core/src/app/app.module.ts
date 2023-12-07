a9f98e67670e3bbb8072caa4db4d6fc5cd33c0d9
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-12-07 16:31:44 -0700

    feat(core):  add persistance moduel

--- packages/core/src/app/app.module.ts
+++ packages/core/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AdminUiModule } from './admin-ui';
 import { ConfigModule } from '../lib/config';
+import { PersistanceModule } from '../lib/persistance';
 
 @Module({
-  imports: [ConfigModule, AdminUiModule]
+  imports: [ConfigModule, PersistanceModule, AdminUiModule]
 })
 export class AppModule {}
---
+++ packages/core/src/lib/persistance/persistance.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DB.URL')
+      }),
+      inject: [ConfigService]
+    })
+  ]
+})
+export class PersistanceModule {}
