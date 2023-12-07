a47abe1e44c37761b34462e9279d63b0491dc2e7
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-12-06 21:46:14 -0700

    chore(theme):  add scripts

--- packages/theme/vite.config.ts
+++ packages/theme/vite.config.ts
@@ -2,7 +2,6 @@ import { resolve } from 'node:path';
 import dts from 'vite-plugin-dts';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as packageJson from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
@@ -19,8 +18,5 @@ export default defineConfig(() => ({
       formats: ['es', 'umd'],
       fileName: format => `index.${format}.js`
     }
-  },
-  rollupOptions: {
-    external: [...Object.keys(packageJson.peerDependencies)]
   }
 }));
