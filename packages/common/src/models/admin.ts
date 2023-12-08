4052c63aa798ba1e295bf0c1b80c2e7854ff6e7b
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-12-07 17:48:40 -0700

    feat(common):  add admin types

---
+++ packages/common/src/models/admin.ts
@@ -0,0 +1,6 @@
+import { Model } from './model';
+
+export interface Admin extends Model {
+  username: string;
+  password: string;
+}
---
+++ packages/common/src/models/model.ts
@@ -0,0 +1,7 @@
+export interface Model {
+  id: ID;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export type ID = string;
