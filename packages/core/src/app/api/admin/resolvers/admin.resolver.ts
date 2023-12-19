b440e8bd9898df3276e7dc5e40f608ae7ec3245a
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-12-18 22:37:25 -0700

    refactor(core):  divide api folder into two, admin and shopp

--- packages/core/src/app/api/admin/resolvers/admin.resolver.ts
+++ packages/core/src/app/api/admin/resolvers/admin.resolver.ts
@@ -1,7 +1,7 @@
 import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
-import { AdminJwtAuthGuard, AuthenticateAdminInput } from '../common';
+import { AdminJwtAuthGuard, AuthenticateAdminInput } from '../../common';
 
 import { AdminService } from '@/app/service';
 
--- packages/core/src/app/api/api.module.ts
+++ packages/core/src/app/api/api.module.ts
@@ -1,49 +1,14 @@
 import { join } from 'path';
 
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { GraphQLModule as NGraphQLModule } from '@nestjs/graphql';
 import { ServeStaticModule } from '@nestjs/serve-static';
 
+import { AdminApiModule } from './admin/admin.module';
 import { DateScalar, IDScalar } from './common/scalars';
-import { AdminResolver } from './resolvers';
-import { ServiceModule } from '../service';
-
-const COMMON_GQL_OPTIONS: ApolloDriverConfig = {
-  driver: ApolloDriver,
-  playground: false,
-  includeStacktraceInErrorResponses: false,
-  plugins: [ApolloServerPluginLandingPageLocalDefault()],
-  formatError: error => {
-    return {
-      message: error.message,
-      code: error.extensions?.code
-    };
-  }
-};
-
-const ADMIN_RESOLVERS = [AdminResolver];
-
-@Module({
-  imports: [ServiceModule],
-  providers: [...ADMIN_RESOLVERS]
-})
-class AdminApiModule {}
 
 @Module({
   imports: [
     AdminApiModule,
-    NGraphQLModule.forRoot<ApolloDriverConfig>({
-      ...COMMON_GQL_OPTIONS,
-      path: '/api/admin',
-      typePaths: [join(process.cwd(), 'src/app/api/**/*.schema.gql')],
-      definitions: {
-        path: join(process.cwd(), 'src/app/api/common/types/gql.types.ts'),
-        outputAs: 'class'
-      },
-      include: [AdminApiModule]
-    }),
     ServeStaticModule.forRoot({
       rootPath: join(process.cwd(), '../admin-ui/dist'),
       serveRoot: '/admin',
