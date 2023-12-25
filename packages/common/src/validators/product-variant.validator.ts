17efd0ca77a5f58e05bed78a470dbe769eedb8f7
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-12-25 00:50:46 -0700

    feat(commong):  add partial validatoer

--- packages/common/src/validators/product-variant.validator.ts
+++ packages/common/src/validators/product-variant.validator.ts
@@ -18,4 +18,15 @@ const Validator = z.object({
   enabled: z.boolean()
 } satisfies MakeAny<ValidateInput>);
 
+const PartialValidator = z.object({
+  price: z.number().min(0),
+  sku: z.string().min(3).max(255),
+  stock: z.number().min(0),
+  weight: z.number().min(0).optional(),
+  comparisonPrice: z.number().min(0).optional(),
+  costPerUnit: z.number().min(0),
+  enabled: z.boolean()
+} satisfies MakeAny<ValidateInput>);
+
 export const validateProductVariant = validatorFactory<ValidateInput>(Validator);
+export const partialValidateProductVariant = validatorFactory<ValidateInput>(PartialValidator);
