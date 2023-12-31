44384ee44e6a8cc7788a534b64af0beb41f1e2fa
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-12-31 12:39:06 -0700

    feat(core):  add asset schema foer prisma

--- packages/core/src/app/storage/storage.module.ts
+++ packages/core/src/app/storage/storage.module.ts
@@ -1,12 +1,8 @@
-import { randomUUID } from 'crypto';
-import { extname } from 'path';
-
 import { Module } from '@nestjs/common';
 
 import { StorageService } from './storage.service';
 
 @Module({
-  imports: [],
   providers: [StorageService],
   exports: [StorageService]
 })
